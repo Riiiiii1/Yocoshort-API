505b525f5743eee78e4f23172f81f2e779e2c515
davidsisa <sisabuestandavidesteban@gmail.com>
2025-12-18 19:10:04 -0500

    fix: el render si agarra wildcard

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,7 @@ use App\Http\Controllers\UrlShortenerController;
 use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\UrlUserController;
 
-/*
-|--------------------------------------------------------------------------
-| RUTAS DE SUBDOMINIOS (COMENTADAS POR LIMITACIÓN DE HOSTING)
-|--------------------------------------------------------------------------
-| Esta lógica está lista, pero requiere un hosting que soporte Wildcard DNS
-| y certificados SSL dinámicos.
-*/
-/*
+$mainApiHost = parse_url(config('app.url'), PHP_URL_HOST); 
 $shortLinkDomain = config('app.short_link_domain', 'yocoshort.com');
 
 Route::domain('{subdomain}.' . $shortLinkDomain)->group(function () {
@@ -21,24 +14,17 @@ Route::domain('{subdomain}.' . $shortLinkDomain)->group(function () {
     });
     Route::get('/{short_code}', [UrlUserController::class, 'handleRedirect']);
 });
-*/
 
-
-// Página de inicio
-Route::get('/', function () {
-    return view('welcome');
+Route::domain($mainApiHost)->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::prefix('auth/google')->group(function () {
+        Route::get('/redirect-web', [GoogleAuthController::class, 'redirectToGoogle'])->name('login.google');
+        Route::get('/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
+    });
+    Route::get('/profile', function () {
+        return view('profile');
+    })->middleware('auth:sanctum');
+    Route::get('/{short_code}', [UrlShortenerController::class, 'redirect'])->name('url.redirect');
 });
-
-// Autenticación con Google
-Route::prefix('auth/google')->group(function () {
-    Route::get('/redirect-web', [GoogleAuthController::class, 'redirectToGoogle'])->name('login.google');
-    Route::get('/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
-});
-
-// Perfil de Usuario (Protegido)
-Route::get('/profile', function () {
-    return view('profile');
-})->middleware('auth:sanctum');
-// Redireccion
-// Redireccionamiento simple
-Route::get('/{short_code}', [UrlShortenerController::class, 'redirect'])->name('url.redirect');
